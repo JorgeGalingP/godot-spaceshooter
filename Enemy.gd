30af1d7cb48e3d550683f03f48baf7f886a30320
Jorge Galindo Peña <jorgape225@gmail.com>
2020-06-21 22:07:02 +0200

    Enemy spawn functionality
    Enemy spawn functionality, added logic to out game and added collision detection to our bodies

--- Enemy.gd
+++ Enemy.gd
@@ -3,6 +3,18 @@ extends Area2D
 export(int) var speed = 20
 export(int) var armor = 3
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.x -= speed * delta
+
+
+func _on_Enemy_body_entered(body):
+	body.queue_free()
+	armor -= 1
+	
+	if armor <= 0:
+		queue_free()
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
--- Ship.gd
+++ Ship.gd
@@ -1,10 +1,30 @@
 extends Area2D
 
+const Bullet = preload("res://Bullet.tscn")
+
 export(int) var speed = 100
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("ui_up"):
 		position.y -= speed * delta
 	if Input.is_action_pressed("ui_down"):
 		position.y += speed * delta
+	if Input.is_action_just_pressed("ui_accept"):
+		fire_bullet()
+
+
+func fire_bullet():
+	var bullet = Bullet.instance()
+	
+	# get main node and attach to it a bullet child
+	var world_node = get_tree().current_scene
+	world_node.add_child(bullet)
+	
+	# set bullet global position into ship global position
+	bullet.global_position = global_position 
+
+
+func _on_Ship_area_entered(area):
+	area.queue_free() # delete the two 2D areas
+	queue_free()
