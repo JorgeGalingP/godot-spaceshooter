16c646042c9f3ef0daf518113eab5a0293573bdd
JorgeGalingP <jorgape225@gmail.com>
2020-06-27 11:13:14 +0200

    Added call_defered on enemy animation

--- Enemy.gd
+++ Enemy.gd
@@ -7,29 +7,29 @@ export(int) var armor = 3
 
 # called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.x -= speed * delta
+    position.x -= speed * delta
 
 
 func _on_Enemy_body_entered(body):
-	body.queue_free()
-	armor -= 1
-	
-	if armor <= 0:
-		var main = get_tree().current_scene
+    body.queue_free()
+    armor -= 1
+    
+    if armor <= 0:
+        var main = get_tree().current_scene
 
-		if main.is_in_group("World"):
-			main.score += 10
-		queue_free()
+        if main.is_in_group("World"):
+            main.score += 10
+        queue_free()
 
 
 func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()
+    queue_free()
 
 
 func _exit_tree():
-	# get main node and attach to it a explosion animation child
-	var world_node = get_tree().current_scene
-	var explosion_effect = ExplosionEffect.instance()
-	world_node.call_deferred("add_child", explosion_effect)
-	# world_node.add_child(explosion_effect)
-	explosion_effect.global_position = global_position
+    # get main node and attach to it a explosion animation child
+    var world_node = get_tree().current_scene
+    var explosion_effect = ExplosionEffect.instance()
+    world_node.call_deferred("add_child", explosion_effect)
+    # world_node.add_child(explosion_effect)
+    explosion_effect.global_position = global_position
