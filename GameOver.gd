a56b173b47a6cd72d78607adb782300eb0a969b5
Jorge Galindo Peña <jorgape225@gmail.com>
2020-07-10 22:55:09 +0200

    Added highscore system

--- GameOver.gd
+++ GameOver.gd
@@ -1,7 +1,18 @@
 extends Node
 
+# Highscore: 0
+onready var highScoreLabel = $HighscoreLabel
+
+func _ready():
+	set_highscore_label()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().change_scene("res://StartMenu.tscn")
+		
+
+func set_highscore_label():
+	var save_data = SaveAndLoad.load_data_from_file()
+	highScoreLabel.text = " Highscore: " + str(save_data.highscore)
--- World.gd
+++ World.gd
@@ -6,10 +6,23 @@ onready var score_label = $ScoreLabel
 
 func set_score(new_score):
 	score = new_score
+	update_score_label()
+
+
+func update_score_label():
 	score_label.text = "Score " + str(score)
 
 
+func update_save_data():
+	var save_data = SaveAndLoad.load_data_from_file()
+	if score > save_data.highscore:
+		save_data.highscore = score
+		SaveAndLoad.save_data_to_file(save_data)
+
+
 func _on_Ship_player_death():
+	update_save_data()
+	
 	# we pause 1 sec so we can view the ship death particles
 	yield(get_tree().create_timer(1), "timeout")
 	get_tree().change_scene("res://GameOver.tscn")
